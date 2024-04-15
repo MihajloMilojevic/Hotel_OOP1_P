443c466e23ba5c44e701a1c8a63308fb1e1c8a26
MihajloMilojevic <milojevicm374@gmail.com>
2024-04-15 06:04:29 +0200

    first commit

---
+++ src/database/Table.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import models.Model;
+
+public abstract class Table {
+	private String tableName;
+	private String primaryKey;
+	private String filePath;
+	private ArrayList<Model> rows;
+	private HashMap<String, HashMap<String, Model>> indecies;
+
+	public Table(String tableName, String primaryKey, String filePath) {
+		this.tableName = tableName;
+		this.primaryKey = primaryKey;
+		this.filePath = filePath;
+		this.rows = new ArrayList<Model>();
+		this.indecies = new HashMap<String, HashMap<String, Model>>();
+		this.AddIndex(primaryKey);
+	}
+
+	public void AddIndex(String indexName) {
+		HashMap<String, Model> newIndex = new HashMap<String, Model>();
+		for (Model row : this.rows) {
+			newIndex.put(row.get(indexName), row);
+		}
+		this.indecies.put(indexName, newIndex);
+	}
+}
---
+++ src/database/queries/QueryData.java
@@ -0,0 +1,11 @@
+package database.queries;
+
+public class QueryData {
+	private String[] select;
+	private String[] from;
+	private String[] where;
+	private String[] groupBy;
+	private String[] having;
+	private String[] orderBy;
+	private String[] limit;
+}
